76bc7486cf6a38285281cee56ac6a1c310643ad2
Stian Kristensen <stian.a.kristensen@gmail.com>
2025-02-05 18:43:56 +0100

    Added another filter

--- functions/processing.py
+++ functions/processing.py
@@ -58,3 +58,67 @@ def velocityFilter(npArray, threshold=0.1):
 
     return np.array([first, second])
 
+def normalLength(line, point):
+    '''
+    Returns the distance from the point to the line.
+    '''
+    p1=np.array(line[0])
+    p2=np.array(line[1])
+    p3=np.array(point)
+
+    return np.cross(p2-p1,p3-p1)/np.linalg.norm(p2-p1)
+
+def turningAndFurthest(npArray, threshold=1):
+    '''
+    First find turning points, then add the points between points of the
+    turning point curve that is the further away than the threshold from the line created by 
+    the turning point curve. Further add points if another point exceeds the threshold again.
+    If points exceeding the threshold is less than the threshold from the previous point, 
+    instead replace the previous point.
+    '''
+    returnList = [[], []]
+
+    #Get turning mask and points
+    dx = np.diff(npArray[1])
+
+    mask = dx[1:] * dx[:-1] < 0
+    mask = np.concatenate(([True], mask, [True])) #Add first and last points
+
+    turningPoints = np.array([np.array(npArray[0])[mask], np.array(npArray[1])[mask]])
+
+    j = 0
+    for i, isTurning in enumerate(mask):
+
+        if isTurning:
+            if (j + 1) == len(turningPoints[0]):
+                break
+
+            line = [[turningPoints[0][j], turningPoints[1][j]], [turningPoints[0][j+1], turningPoints[1][j+1]]]
+
+            j += 1
+
+            extraPoints = []
+            lastDistance = 0
+            for k, point in enumerate(npArray[0][(i + 1):], 1):
+
+                if mask[(i + k)] == True:
+                    break
+
+                middlePoint = [point, npArray[1][i + k]]
+
+                distance = normalLength(line, middlePoint)
+
+                if abs(distance - lastDistance) > threshold:
+                    #Add point
+                    extraPoints.append(middlePoint)
+                    lastDistance = distance
+
+                elif lastDistance != 0 and distance > lastDistance:
+                    #Replace point
+                    extraPoints[-1] = middlePoint
+
+            returnList[0] += ([npArray[0][i]] + [p[0] for p in extraPoints])
+            returnList[1] += ([npArray[1][i]] + [p[1] for p in extraPoints])
+
+    return np.array(returnList, dtype=int)
+
