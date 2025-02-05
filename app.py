ddd45eacb468234adfcefda273f312e39a08ee70
Stian Kristensen <stian.a.kristensen@gmail.com>
2025-02-05 19:51:54 +0100

    Added a starting countdown

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 import signal
+import time
 import functions.funscript as funscript
 
 
 baudRate = 9600 #bits per second
 maxAnalog = 1023 #Android nano 10bits > 2^10 = 1024 - 1 = 1023 (as signal starts at 0)
 sampleFrequency = 100 #times per second
+
+
+def countdown(seconds=3):
+    input('Enter anything to start recording:\n')
+
+    print('Starting in:')
+    time.sleep(1)
+
+    for i in range(seconds):
+        print(f'{seconds - i}...')
+        time.sleep(1)
 
 
 if __name__ == '__main__':
@@ -17,5 +29,7 @@ if __name__ == '__main__':
     signal.signal(signal.SIGINT, shutdown)
     signal.signal(signal.SIGTERM, shutdown)
 
-    scripter.startRecording(length=10, print2terminal=True)
+    countdown()
+
+    scripter.startRecording(length=5, print2terminal=True)
 
