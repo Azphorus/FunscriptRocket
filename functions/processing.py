18728bc2cdf29f4ed1aadd491bb5b4d4f9f5e80c
Stian Kristensen <stian.a.kristensen@gmail.com>
2025-02-05 16:07:23 +0100

    Added turning points filter

--- functions/processing.py
+++ functions/processing.py
@@ -2,14 +2,14 @@ import numpy as np
 import scipy.ndimage
 import rdppy
 
-def removeOutliers(dataArray, range, index=1):
+def removeOutliers(npArray, size=3, index=1):
     '''
     Remove outliers in the specified index of the given 
     data array using a median filter.
     '''
-    dataArray[index] = scipy.ndimage.median_filter(dataArray[index], size=range)
+    npArray[index] = scipy.ndimage.median_filter(npArray[index], size=size)
 
-def rdpAlgorithm(dataArray, threshold=0.9):
+def rdpAlgorithm(npArray, threshold=0.9):
     '''
     Vectorises the curve, removing points that follow straight lines 
     based on threshold value.
@@ -17,6 +17,18 @@ def rdpAlgorithm(dataArray, threshold=0.9):
     returns processed array
     '''
     
-    mask = rdppy.filter(dataArray.T, threshold)
-    return np.array([np.array(dataArray[0])[mask], np.array(dataArray[1])[mask]])
+    mask = rdppy.filter(npArray.T, threshold)
+    return np.array([np.array(npArray[0])[mask], np.array(npArray[1])[mask]])
+
+def turningPoints(npArray):
+    '''
+    Return an array only containing the turning points (where the derivative changes)
+    '''
+
+    dx = np.diff(npArray[1])
+
+    first = np.extract(dx[1:] * dx[:-1] < 0, npArray[0])
+    second = np.extract(dx[1:] * dx[:-1] < 0, npArray[1])
+
+    return np.array([first, second])
 
